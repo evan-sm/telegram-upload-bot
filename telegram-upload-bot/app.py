2f016d6f417ead59d54b488efe556136a48f1844
wmw <wmwofficial@gmail.com>
2018-08-17 15:20:52 +0300

    0

--- telegram-upload-bot/app.py
+++ telegram-upload-bot/app.py
@@ -62,7 +62,7 @@ def main():
     dp.add_handler(CommandHandler("help", help))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(MessageHandler(Filters.chat(144149077) & Filters.text, echo))
 
     # log all errors
     dp.add_error_handler(error)
